46e20f7e1d989d31ebbef0dc936cde37fceec9b3
billy7175 <whgus439@gmail.com>
2024-01-03 15:44:19 +0900

    Refactor dashboard api

--- services/dashboard/index.js
+++ services/dashboard/index.js
@@ -1,26 +1,32 @@
-const User = require('../../schemas/users') 
-const Class = require('../../schemas/classes')
-const Question = require('../../schemas/questions')
+const User = require('../../schemas/users');
+const Class = require('../../schemas/classes');
+const Question = require('../../schemas/questions');
 
-async function getDashboard(req, res){
-    const users = await User.find({}).exec()
-    const classes = await Class.find({}).exec()
-    const questions = await Question.find({}).exec()
-    const studentCount = users.filter(user => user.role === 'student').length
-    const teacherCount = users.filter(user => user.role === 'teacher').length
-    const classCount = classes.length
-    const questionCount = questions.length
+async function getDashboard(req, res) {
+    try {
+        const users = await User.find({});
+        const classes = await Class.find({});
+        const questions = await Question.find({});
 
-    return res.send(
-        {
-            teacherCount : teacherCount, 
-            studentCount : studentCount,
-            classCount : classCount,
-            questionCount : questionCount
-        }
-    )
+        const studentCount = users.filter(user => user.role === 'student').length;
+        const teacherCount = users.filter(user => user.role === 'teacher').length;
+        const classCount = classes.length;
+        const questionCount = questions.length;
+
+        const dashboardData = {
+            teacherCount,
+            studentCount,
+            classCount,
+            questionCount
+        };
+
+        return res.status(200).json(dashboardData);
+    } catch (error) {
+        console.error('Error in getDashboard:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
 
 module.exports = {
-    getDashboard : getDashboard
-} 
+    getDashboard
+};
