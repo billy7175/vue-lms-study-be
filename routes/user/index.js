1e58d7ed9603e8d86009738f740c824778cb6fdd
billy7175 <whgus439@gmail.com>
2024-02-01 16:42:11 +0900

    Feat: add getUserById api

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const { getUsers, login } = require('../../services/user')
-const { requireSignin } = require('../../middlewares/auth')
-console.log('#requireSignin', requireSignin)
+const express = require("express");
+const router = express.Router();
+const { getUserById, getUsers, login } = require("../../services/user");
+const { requireSignin } = require("../../middlewares/auth");
+console.log("#requireSignin", requireSignin);
 
-router.get('/api/users', requireSignin, getUsers)
-router.post('/api/login', login)
+router.get("/api/users", requireSignin, getUsers);
+router.get("/api/users/:id", requireSignin, getUserById);
+router.post("/api/login", login);
 
-
-module.exports = router
+module.exports = router;
--- services/user/index.js
+++ services/user/index.js
@@ -1,69 +1,86 @@
-const User = require('../../schemas/users')
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose");
+const User = require("../../schemas/users");
+const jwt = require("jsonwebtoken");
 
+async function getUserById(req, res) {
+  const id = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(400).json({ error: "Invalid ObjectId format" });
+
+  try {
+    const user = await User.findOne({ _id: id }).exec();
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 async function getUsers(req, res) {
-    console.log('#getUsers')
-    try {
-        const Users = await User.aggregate([
-            // {
-            //     // $match : {
-            //     //     name : 'test'
-            //     // }
-            // },
-            // { $limit: 1 },
-            {
-                $lookup: {
-                    from: "classes",
-                    localField: 'classTest',
-                    foreignField: "_id",
-                    as: "class"
-                }
-            },
-            {
-                $addFields : {
-                    class:  { $arrayElemAt: ["$class", 0] }  // Take the first element if the array is not empty
-                }
-            }
-            
-        ])
-    
-        res.status(200).send(Users)
-    } catch(error){
-        console.log(error)
-    }
+  console.log("#getUsers");
+  try {
+    const Users = await User.aggregate([
+      // {
+      //     // $match : {
+      //     //     name : 'test'
+      //     // }
+      // },
+      // { $limit: 1 },
+      {
+        $lookup: {
+          from: "classes",
+          localField: "classTest",
+          foreignField: "_id",
+          as: "class",
+        },
+      },
+      {
+        $addFields: {
+          class: { $arrayElemAt: ["$class", 0] }, // Take the first element if the array is not empty
+        },
+      },
+    ]);
+
+    res.status(200).send(Users);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function login(req, res) {
-    const { email, password } = req.body
-    const existingUser = await User.findOne({
-        email,
-        password
-    }).exec()
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({
+    email,
+    password,
+  }).exec();
 
-    if (existingUser) {
-        const token = jwt.sign({ email: existingUser.email }, process.env.JWT_SECRET, {
-            expiresIn: '7d'
-        })
-        const user = {
-            _id: existingUser._id.valueOf(),
-            name: existingUser.name,
-            email: existingUser.email,
-            role: existingUser.role
-        }
-        return res.json({
-            user: user,
-            token: token
-        })
-    }
+  if (existingUser) {
+    const token = jwt.sign(
+      { email: existingUser.email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+    const user = {
+      _id: existingUser._id.valueOf(),
+      name: existingUser.name,
+      email: existingUser.email,
+      role: existingUser.role,
+    };
+    return res.json({
+      user: user,
+      token: token,
+    });
+  }
 
-    return res.status(400).send({
-        code: 'IAM001',
-        message: 'Please check your account information'
-    })
-
+  return res.status(400).send({
+    code: "IAM001",
+    message: "Please check your account information",
+  });
 }
 
 module.exports = {
-    getUsers,
-    login
-} 
+  getUserById,
+  getUsers,
+  login,
+};
